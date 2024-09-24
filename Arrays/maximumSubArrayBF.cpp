3755b46f04518a8b0dca1721a4b38fa655810f14
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-09-25 00:19:32 +0530

    initial commit

---
+++ Arrays/maximumSubArrayBF.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int max = INT_MIN;
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                int sum = 0;    // reset sum before each sub array
+                for(int k=i; k<=j; k++){
+                    sum += nums[k]; // sum the value not the index k
+                    if(sum > max){
+                        max = sum;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+};
+
+int main(){
+    vector<int> arr = {-2,1,-3,4,-1,2,1,-5,4};
+    Solution sol;
+    cout<<sol.maxSubArray(arr);
+
+    return 0;
+}
